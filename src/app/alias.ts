5aa671994c41025810bc7dd24bf91b404cf6f19e
Karolis Blazevicius <broliukas@gmail.com>
2024-11-21 15:51:38 +0200

    Fix using EMA - esm-module-alias package.

---
+++ src/app/alias.ts
@@ -0,0 +1,7 @@
+import * as url from 'url'
+import generateAliasesResolver from 'esm-module-alias'
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+export const resolve = generateAliasesResolver({
+  app: __dirname,
+})
